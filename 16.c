de4f0a08d8c38cb3914574fc6ca8f03711ee18da
TanPat <tanishpatil30xe@gmail.com>
2024-11-20 22:12:35 +0530

    Update 16.c

--- 16.c
+++ 16.c
@@ -6,12 +6,12 @@ int main()
     int input, sum = 0;
     printf("Input number of terms : ");
     scanf("%d", &input);
-    printf("The odd numbers are: ");
+    printf("The even numbers are: ");
     for (int i = 2; i <= input * 2; i += 2)
     {
         printf("%d ", i);
         sum += i;
     }
-    printf("\nThe Sum of odd Natural Number upto %d terms: %d", input, sum);
+    printf("\nThe Sum of even Natural Number upto %d terms: %d", input, sum);
     return 0;
 }
